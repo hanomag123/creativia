6aee3f60b9cbbfe90d3400d54ee5154e7663b780
hanomag123 <87242345+hanomag123@users.noreply.github.com>
2022-02-06 15:24:52 +0300

    scroll

---
+++ script/script.js
@@ -0,0 +1,36 @@
+let arrRight = document.querySelector('.header__arrowRight'),
+    arrLeft = document.querySelector('.header__arrowLeft')
+    elem = document.querySelector('.header__main__container'),
+    value = 0,
+    arr = [];
+
+elemL = elem.cloneNode(true);
+elemL.style.transform = 'translate(100vh)';
+elemL.style.position = 'absolute'
+
+elem.parentElement.append(elemL)
+
+elemR = elem.cloneNode(true);
+elemR.style.transform = 'translate(-100vh)';
+elemR.style.position = 'absolute'
+
+elem.parentElement.append(elemR)
+
+
+arr = document.querySelectorAll('.header__main__container');
+
+arrRight.onclick = function() {
+    if (value === -100) {
+        value = 0
+    }
+    value -= 100;
+    elem.parentElement.style.transform = `translate(${value}vh)`;
+}
+arrLeft.onclick = function() {
+    if (value === 100) {
+        value = 0
+    }
+    value += 100;
+    elem.parentElement.style.transform = `translate(${value}vh)`;
+}
+
