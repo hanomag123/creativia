85fac1b4cfdb9a3ffb3567b72c153977feda0130
hanomag123 <87242345+hanomag123@users.noreply.github.com>
2022-02-06 15:32:34 +0300

    scroll update

--- script/script.js
+++ script/script.js
@@ -5,13 +5,13 @@ let arrRight = document.querySelector('.header__arrowRight'),
     arr = [];
 
 elemL = elem.cloneNode(true);
-elemL.style.transform = 'translate(100vh)';
+elemL.style.transform = 'translate(100vw)';
 elemL.style.position = 'absolute'
 
 elem.parentElement.append(elemL)
 
 elemR = elem.cloneNode(true);
-elemR.style.transform = 'translate(-100vh)';
+elemR.style.transform = 'translate(-100vw)';
 elemR.style.position = 'absolute'
 
 elem.parentElement.append(elemR)
@@ -24,13 +24,13 @@ arrRight.onclick = function() {
         value = 0
     }
     value -= 100;
-    elem.parentElement.style.transform = `translate(${value}vh)`;
+    elem.parentElement.style.transform = `translate(${value}vw)`;
 }
 arrLeft.onclick = function() {
     if (value === 100) {
         value = 0
     }
     value += 100;
-    elem.parentElement.style.transform = `translate(${value}vh)`;
+    elem.parentElement.style.transform = `translate(${value}vw)`;
 }
 
